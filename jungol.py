fe5a6e509a48669bbbad7a9b1964af391d63c7bc
ppakkk <110581818+ppakkk@users.noreply.github.com>
2022-09-15 19:36:47 +0900

    update

---
+++ jungol.py
@@ -0,0 +1,60 @@
+#출력 - 자가진단 1
+#print("Python")
+
+#출력 -자가진단 2-1
+# str1="Score:"
+# str2="100"
+# str3="GREAT!"
+# print(str1)
+# print(str2)
+# print(str3)
+
+#출력 - 자가진단 2-2
+# print("My height")
+# print("170")
+# print("My weight")
+# print("68.6")
+
+#출력 - 자가진단 3
+# print('''1plus1 = Gwi yo mi''')
+
+#출력 - 자가진단 4
+# print('''(@) (@)''')
+# print('''(=^.^=)''')
+# print('''(-m-m-)''')
+
+#출력 - 자가진단 5-1
+# print('''5 Dan''')
+# print("5 * 2 =", 5*2)
+#printf("5 * 2 = {}".format(5*2))
+
+#출력 - 자가진단 5-2
+# print("6 + 2 =", 6 + 2)
+# print("6 - 2 =", 6 - 2)
+# print("6 * 2 =", 6 * 2)
+# print("6 / 2 =", 6 / 2)
+
+#출력 - 형성평가 1
+# print("My name is Hong")
+
+#출력 - 형성평가 2
+# print("My hometown\nFlowering mountain")
+
+#출력 - 형성평가 3
+# print("TTTTTTTTTT")
+# print("TTTTTTTTTT")
+# print("    TT")
+# print("    TT")
+# print("    TT")
+
+#출력 - 형성평가 4
+# import math
+# print("kor 90")
+# print("mat 80")
+# print("eng 100")
+# print("sum", 90+80+100)
+# print("avg", math.trunc((90+80+100)/3))
+
+#변수와 입력 - 자가진단 1
+a =-100
+print()
