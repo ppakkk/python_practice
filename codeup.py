42e23420b70b3e8d09e48d021e6490b5c8c125b6
ppakkk <110581818+ppakkk@users.noreply.github.com>
2022-11-22 12:00:54 +0900

    update

---
+++ codeup.py
@@ -0,0 +1,15 @@
+#Codeup - 6001
+# print("Hello")
+
+#Codeup - 6002
+# print("Hello","World")
+
+#Codeup - 6003
+# print("Hello")
+# print("World")
+
+#Codeup - 6004
+# print("'Hello'")
+
+#Codeup - 6005
+print('"Hello World"')
